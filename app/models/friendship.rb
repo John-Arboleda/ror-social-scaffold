a30a33eadac5de146a63c79b625bd111787d57fb
John-Arboleda <johnjack0126@gmail.com>
2021-09-13 07:20:35 -0500

    Add associations friendships model, update views

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,4 @@
 class Friendship < ApplicationRecord
-  belongs_to :sender
-  belongs_to :receiver
+  belongs_to :sender, foreign_key: 'sender_id', class_name: 'User'
+  belongs_to :receiver, foreign_key: 'receiver_id', class_name: 'User'
 end
