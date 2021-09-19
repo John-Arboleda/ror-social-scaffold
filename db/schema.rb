6b807a77bf877eae0f66af8b87219e57c14047a8
John-Arboleda <johnjack0126@gmail.com>
2021-09-19 07:50:12 -0500

    Migrate friendship table updated column names

--- db/schema.rb
+++ db/schema.rb
@@ -26,13 +26,13 @@ ActiveRecord::Schema.define(version: 2021_09_13_115814) do
   end
 
   create_table "friendships", force: :cascade do |t|
-    t.bigint "sender_id", null: false
-    t.bigint "receiver_id", null: false
-    t.integer "status", default: 0
+    t.bigint "invitation_sender_id", null: false
+    t.bigint "invitation_receiver_id", null: false
+    t.boolean "status", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["receiver_id"], name: "index_friendships_on_receiver_id"
-    t.index ["sender_id"], name: "index_friendships_on_sender_id"
+    t.index ["invitation_receiver_id"], name: "index_friendships_on_invitation_receiver_id"
+    t.index ["invitation_sender_id"], name: "index_friendships_on_invitation_sender_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -64,6 +64,6 @@ ActiveRecord::Schema.define(version: 2021_09_13_115814) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "friendships", "users", column: "receiver_id"
-  add_foreign_key "friendships", "users", column: "sender_id"
+  add_foreign_key "friendships", "users", column: "invitation_receiver_id"
+  add_foreign_key "friendships", "users", column: "invitation_sender_id"
 end
