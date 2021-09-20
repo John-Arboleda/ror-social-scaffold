8abf2ae48bfd9d6dabf9e290994dbebe389f4b3b
John-Arboleda <johnjack0126@gmail.com>
2021-09-19 19:02:18 -0500

    Update user and friendship views

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,30 +10,32 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  has_many :invitations_received, foreign_key: :invitation_sender_id, class_name: 'Friendship', dependent: :destroy
+  has_many :invitations_received, foreign_key: :invitation_receiver_id, class_name: 'Friendship', dependent: :destroy
   has_many :users_who_invited_me, through: :invitations_received, source: :invitation_sender, dependent: :destroy
 
-  has_many :invitations_sent, foreign_key: :invitation_receiver_id, class_name: 'Friendship', dependent: :destroy
+  has_many :invitations_sent, foreign_key: :invitation_sender_id, class_name: 'Friendship', dependent: :destroy
   has_many :users_invited_by_me, through: :invitations_sent, source: :invitation_receiver, dependent: :destroy
 
-  def friendship_requests(user)
+  def friendship_request(user)
     return false if users_invited_by_me.include?(user) || users_who_invited_me.include?(user)
 
     users_invited_by_me << user
   end
 
   def accept_request(user)
-    friend_request = invitation_list.where(invitation_sende_id: user.id).first
+    friend_request = invitations_received.where(invitation_sender_id: user.id).first
     friend_request.update(status: true)
   end
 
   def reject_request(user)
-    friend_request = invitation_list.where(invitation_sende_id: user.id).first
+    friend_request = invitations_received.where(invitation_sender_id: user.id).first
     friend_request.destroy
   end
 
   def friends
-    User.where(id: invitation_list.where(status: true).pluck(:invitation_sender_id))
+    friend_sender = User.where(id: invitations_sent.where(status: true).pluck(:invitation_receiver_id))
+    friend_receiver = User.where(id: invitations_received.where(status: true).pluck(:invitation_sender_id))
+    is_a_friend = friend_sender + friend_receiver
   end
 
   def friend?(user)
